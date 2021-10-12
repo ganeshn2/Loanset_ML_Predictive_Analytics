40edd669361cc2de43e563e0a8d518f44e9285bc
Ganesh Narayanan <ganeshncsu@gmail.com>
2021-10-11 21:13:45 -0400

    Update loanSet_ML_Predictive_Analytics.py

--- scripts/loanSet_ML_Predictive_Analytics.py
+++ scripts/loanSet_ML_Predictive_Analytics.py
@@ -9,24 +9,29 @@ from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
 
+
+
 def loan_dataframe():
-    df = pd.read_csv('./input/Loanset.csv', index_col=[0])
-    df = df.drop(columns=['Unnamed: 0.1', 'Unnamed: 0.1.1'], axis=1)
-    df['effective_date'] = pd.to_datetime(df['effective_date'])
-    df['due_date'] = pd.to_datetime(df['due_date'])
-    df['Dayofweek'] = df['due_date'].dt.dayofweek
-    df = df.drop(['effective_date', 'due_date'], axis=1)
-    df['weekend'] = df['Dayofweek'].apply(lambda x: 1 if (x > 4) else 0)
-    return df
+    loan_df = pd.read_csv('./input/Loanset.csv', index_col=[0])
+    loan_df = loan_df.drop(columns=['Unnamed: 0.1', 'Unnamed: 0.1.1'], axis=1)
+    loan_df['effective_date'] = pd.to_datetime(loan_df['effective_date'])
+    loan_df['due_date'] = pd.to_datetime(loan_df['due_date'])
+    loan_df['Dayofweek'] = loan_df['due_date'].dt.dayofweek
+    loan_df = loan_df.drop(['effective_date', 'due_date'], axis=1)
+    loan_df['weekend'] = loan_df['Dayofweek'].apply(lambda x: 1 if (x > 4) else 0)
+    return loan_df
 
 
 def cat_to_cont(df):
-    df1 = pd.get_dummies(df['education','Gender'],drop_first = False)
-    return df1
+    df1 = pd.get_dummies(loan_df['education'], drop_first=False)
+    df2 = pd.get_dummies(loan_df['Gender'],drop_first=True)
+    df3 = pd.concat([df1, df2], axis=1)
+    return df3
+
 
 
 
 if __name__ == "__main__":
     loan_dataframe()
-    print(loan_dataframe().head())
-    #cat_to_cont(df)
+    print(loan_dataframe(loan_df))
+    cat_to_cont()
