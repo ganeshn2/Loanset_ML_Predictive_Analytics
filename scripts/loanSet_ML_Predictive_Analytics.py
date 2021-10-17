639bf318e98104670831f9cc2dccc65c57e1f5af
Ganesh Narayanan <ganeshncsu@gmail.com>
2021-10-16 20:23:10 -0400

    Update loanSet_ML_Predictive_Analytics.py

--- scripts/loanSet_ML_Predictive_Analytics.py
+++ scripts/loanSet_ML_Predictive_Analytics.py
@@ -8,7 +8,8 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.svm import SVC
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import train_test_split
-
+from sklearn.metrics import classification_report,confusion_matrix,jaccard_score,\
+    accuracy_score,f1_score,precision_score,recall_score
 
 
 def loan_dataframe():
@@ -28,8 +29,57 @@ def cat_to_cont(df):
     df3 = pd.concat([df1, df2], axis=1)
     return df3
 
+#
+# def facet_grid(data,col,hue,palette,height, aspect,margin_titles,X,title):
+#     plt.Figure(figsize=(12, 6), dpi=1000)
+#     a = sns.FacetGrid(data, col, hue, palette, height, aspect,
+#                       margin_titles)
+#     a.map(plt.hist, 'X')
+#     plt.legend()
+#     plt.tight_layout()
+#     plt.savefig(title + ".jpg")
+#     plt.show()
+#
+#
+# def joint_plot(y,x,data,kind,title):
+#     # a = sns.jointplot(y='age', x='Principal', data=loan_df, kind='scatter')
+#     a = sns.jointplot(y, x, data, kind=kind)
+#     plt.tight_layout()
+#     plt.savefig(title + ".jpg")
+#     plt.show()
+#
+#
+# def pair_plot(df, hue, palette,title):
+#     # grid = sns.pairplot(loan_df, hue='terms', palette='Set3')
+#     grid = sns.pairplot(df, hue=hue, palette=palette)
+#     grid = grid.map_upper(plt.scatter, color='darkred')
+#     grid = grid.map_diag(plt.hist, bins=10, color='blue',
+#                          edgecolor='k')
+#     grid = grid.map_lower(sns.kdeplot, cmap='Reds')
+#     plt.tight_layout()
+#     plt.savefig(title + ".jpg")
+#     plt.show()  # reference: https://towardsdatascience.com/visualizing-data-with-pair-plots-in-python-f228cf529166
+#
+#
+# def kde_plot(x,y,cmap,a,b,c,d):
+#     # kde = sns.kdeplot(loan_df5['terms'], loan_df5['age'],
+#                 #cmap="plasma", shade=True, shade_lowest=False)
+#     kde = sns.kdeplot(x,y,cmap = cmap, shade = True, shade_lowest = False)
+#     plt.xlabel(x, fontsize=15)
+#     plt.ylabel(y, fontsize=15)
+#     plt.xticks(fontsize=12)
+#     plt.yticks(fontsize=12)
+#     plt.xlim(a,b)
+#     plt.ylim(c,d)
+#
+#
+# X = loan_df5.drop(['loan_status'], axis =1)
+# y = loan_df5['loan_status']
 
 if __name__ == "__main__":
     loan_dataframe()
-    print(loan_dataframe(loan_df))
-    cat_to_cont()
+    print(loan_dataframe().head())
+    cat_to_cont(df)
+    # facet_grid(loan_df,Gender,loan_status,Set1,4,1.5,True,Principal)
+    # facet_grid(loan_df,Gender,loan_status,Set1,4,1.5,True,age)
+    # facet_grid(loan_df, Gender, loan_status, Set3, 4, 1.5, True,Dayofweek)
